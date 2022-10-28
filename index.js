ed7b8f61cb27b4d273e6235a1edd4f11ca14e4e4
PedroVitor-oss <pedro.santos22@escola.pr.edu.br>
2022-10-28 16:54:38 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT||3000;
+
+//configuração de handlebars
+const handlebars	= require('express-handlebars');
+app.engine('handlebars', handlebars.engine('main'));
+app.set('view engine','handlebars') ;
+app.set('views','./view');
+//arquivos estaticos
+const path = require('path');
+app.use(express.static(path.join(__dirname,'public')));
+
+
+app.get('/',(req,res)=>{
+    res.render("login");
+})
+
+app.listen(port,console.log("aberto e funcionando "))
